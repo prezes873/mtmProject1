ee4d2f5e39d0fe8774c79d126a6110bce5692735
prezes873 <maciekstodulski@o2.pl>
2015-12-14 13:50:58 +0100

    Revert "Revert "test""
    This reverts commit 10f7c4cd8b08e84625ca8d64a6b302eb6192c081.

---
+++ app/src/main/java/com/example/maciek/mtmproject/Object.java
@@ -0,0 +1,18 @@
+package com.example.maciek.mtmproject;
+
+/**
+ * Created by maciek on 2015-12-13.
+ */
+public class Object {
+
+    String nazwa;
+    float lat, lgn;
+
+
+    public  Object(float lgn, float lat, String nazwa)
+    {
+        this.lat = lat;
+        this.lgn = lgn;
+        this.nazwa = nazwa;
+    }
+}
